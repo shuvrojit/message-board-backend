602e4bb64b0b9a92b6015b46e9aaf7c6c9541ef4
Shuvrojit Biswas <jsuvro17@gmail.com>
2023-09-11 21:29:52 +0600

    Add cors and morgan

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import express, { Express } from "express";
 import messageRoutes from "./routes";
 import bodyParser from "body-parser";
+import cors from "cors";
+import morgan from "morgan";
 
 const app: Express = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors);
+app.use(morgan("combined"));
 
 app.use("/messages", messageRoutes);
 
